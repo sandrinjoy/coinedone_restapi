900a53cfdf358c4b08ac96bc2eb3a83a5838e473
sandrinejoy <sandrinejoy007@gmail.com>
2021-10-21 23:21:52 +0530

    heroku setup

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 require('dotenv').config()
+const  cors = require('cors')
 const express =require('express')
+
 const app = express()
 
 const mongoose =require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true })
 const db=mongoose.connection
 db.on('error',(e)=>{
     console.error(e)
 })
 
 db.once('open',()=>{
-    console.error('Database Connected')
+    console.log('Database Connected')
 })
 
+app.use(cors())
+
+//to accept json reqs
+
 app.use(express.json())
+
+app.get('/test',(req,res)=>{
+    res.send("hi")
+})
+
 app.listen(3000,()=>{
     console.log("server started")
 })
